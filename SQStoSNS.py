e3f3fb5742f747c43806b207c9d926a435467627
Deathnoval <badao867@gmail.com>
2022-12-07 12:38:31 +0700

    SQS to SNS

---
+++ SQStoSNS.py
@@ -0,0 +1,21 @@
+import json
+import boto3
+import os
+ 
+def send_request(body):
+    # Create an SNS client
+    sns = boto3.client('sns')
+    response = sns.publish(
+        TargetArn='arn:aws:sns:us-east-1:407415326349:Bao-Cao-Cloud',    
+        Message=body,    
+    )
+    print('(Debug)Gui thanh cong tn : ' + body)
+    
+ 
+def lambda_handler(event, context):
+    #batch_processes=[]
+    for record in event['Records']:
+        print (record)
+        send_request(record["body"])
+    
+ 
